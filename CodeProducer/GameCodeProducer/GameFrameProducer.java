5833a4b5d0cae586b735739def3c7263752273c2
Sammy Li <sammyli20082000@yahoo.com.hk>
2016-03-02 17:03:16 +0800

    formating

--- CodeProducer/GameCodeProducer/GameFrameProducer.java
+++ CodeProducer/GameCodeProducer/GameFrameProducer.java
@@ -7,40 +7,52 @@ public class GameFrameProducer {
 	}
 	
 	public void printGameJavaFrame() {
-		String codes[] = {
-				"package Executable;",
-				
-				"import java.io.*;",
-				"import java.util.*;",
-				
-				"import Executable.BoardModel.Board;",
-				"import Executable.BoardModel.Point;",
-				"import Executable.FileHandlerModel.FileHandler;",
-				"import Executable.PieceModel.Piece;",
-				"import Executable.UIHandlerModel.UIHandler;",
-				
-				"public class Game implements Serializable {",
-				
-				"Game game;",
-				"Board board;",
-				"AI ai;",
-				"boolean canCapture;",
-				"ArrayList<String> sides = new ArrayList<>();",
-				"String currentSide;",
-				"ArrayList<Piece> currPieces;",
-				"ArrayList<Node> history;",
-				"Piece selectedPiece;",
-				"Point selectedPoint;",
-				"UIHandler ui;",
-				"String gameLocation;",
-				
-				"public static void main(String[] args) {",
-				"new Game();",
-				"}",
-				
-				
-				
-				"}"
-		};
+		GameCodeProducer.gameCodes.add("package Executable;");
+		
+		GameCodeProducer.gameCodes.add("import java.io.*;");
+		GameCodeProducer.gameCodes.add("import java.util.*;");
+		
+		GameCodeProducer.gameCodes.add("import Executable.BoardModel.Board;");
+		GameCodeProducer.gameCodes.add("import Executable.BoardModel.Point;");
+		GameCodeProducer.gameCodes.add("import Executable.FileHandlerModel.FileHandler;");
+		GameCodeProducer.gameCodes.add("import Executable.PieceModel.Piece;");
+		GameCodeProducer.gameCodes.add("import Executable.UIHandlerModel.UIHandler;");
+		
+		GameCodeProducer.gameCodes.add("public class Game implements Serializable {");
+		
+		GameCodeProducer.gameCodes.add("Game game;");
+		GameCodeProducer.gameCodes.add("Board board;");
+		GameCodeProducer.gameCodes.add("AI ai;");
+		GameCodeProducer.gameCodes.add("boolean canCapture = true;");
+		GameCodeProducer.gameCodes.add("ArrayList<String> sides = new ArrayList<>();");
+		GameCodeProducer.gameCodes.add("String currentSide;");
+		GameCodeProducer.gameCodes.add("ArrayList<Piece> currPieces;");
+		GameCodeProducer.gameCodes.add("ArrayList<Node> history;");
+		GameCodeProducer.gameCodes.add("Piece selectedPiece;");
+		GameCodeProducer.gameCodes.add("Point selectedPoint;");
+		GameCodeProducer.gameCodes.add("UIHandler ui;");
+		GameCodeProducer.gameCodes.add("String gameLocation;");
+		
+		GameCodeProducer.gameCodes.add("public static void main(String[] args) {");
+		GameCodeProducer.gameCodes.add("new Game();");
+		GameCodeProducer.gameCodes.add("}");
+		
+		GameCodeProducer.gameCodes.add("public Game()");
+		
+		GameCodeProducer.gameCodes.add("private void addPointsPiecesEdgesToBoard()");
+		
+		GameCodeProducer.gameCodes.add("private FileHandler.EventCallBackHandler handleFileEventCallBack()");
+		
+		GameCodeProducer.gameCodes.add("private UIHandler.EventCallBackHandler handleUIEventCallBack()");
+		
+		GameCodeProducer.gameCodes.add("private void handleMenuBarMessage(UIHandler.MenubarMessage msg)");
+		
+		GameCodeProducer.gameCodes.add("private void programTerminate()");
+		
+		GameCodeProducer.gameCodes.add("public void computerMakeMove()");
+		
+		GameCodeProducer.gameCodes.add("private void addDataToInfoPanel()");
+		
+		GameCodeProducer.gameCodes.add("}");
 	}
 }
